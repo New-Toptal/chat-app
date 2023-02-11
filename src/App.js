b34584d1766a06ca15bc30b7a7fdfe9cea42f610
superstar <123185460+superstar320star@users.noreply.github.com>
2023-02-11 15:35:36 +0900

    UI draft completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Register from './components/register';
+import Login from './components/login';
+import Forgot from './components/forgot';
+import Chatboard from './components/chatBoard/index';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/chat' element={<Chatboard />} />
+        <Route path='/forgot' element={<Forgot />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
